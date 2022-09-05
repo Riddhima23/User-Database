66950cbc61620730c9cc97f3ac6ef6448e53201d
Riddhima Singh <58878761+Riddhima23@users.noreply.github.com>
2022-09-05 11:02:21 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=user_info', 
-   'arin', 'majumder');
+   'riddhima', 'singh');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
